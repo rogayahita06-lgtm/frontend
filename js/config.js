b9466fb691a18e65c3a156787acd51d7f11e2973
rogayahita06-lgtm <rogayahita06@gmail.com>
2026-01-22 17:54:21 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
-export const API_BASE = "http://localhost:3000";
+export const API_BASE = "https://backend-seven-psi-29.vercel.app/";
 export const SUPABASE_URL = "https://ulpsstljbbkdkzncpbzx.supabase.co";
 export const SUPABASE_ANON_KEY = "sb_publishable_DBe42eN4ZBLGGDsyKyOZYw_Koc-IeeW";
